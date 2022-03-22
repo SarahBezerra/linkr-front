e6ae2b08959845e33d6f1c9864a51b180018e9ff
Yara Tavares <yaracriistavares@gmail.com>
2022-03-22 19:48:21 -0300

    feat: get likes of the post and the user

--- src/pages/Timeline/LikeHeart/index.js
+++ src/pages/Timeline/LikeHeart/index.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import api from "../../../services";
 import { ContentLikes, IconHeartRed, IconHeartDefault } from "./style";
 
-export default function LikeHeart() {
-  const [like, setLike] = useState(false);
+export default function LikeHeart({ idPost, idStatus }) {
+  const [like, setLike] = useState(idStatus || false); //pegar informação se post está com like
 
   async function likeOrNot() {
     setLike(!like);
+
+    api.postLikeOrNot(idPost);
   }
 
   return (
---
+++ src/services/index.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000";
+
+function createConfig(token) {
+  return { headers: { Authorization: `Bearer ${token}` } };
+}
+
+async function getLikes() {
+  return axios.post(`${BASE_URL}/like/1`);
+}
+
+async function postLikeOrNot(idPost) {
+  return axios.post(`${BASE_URL}/like/1`, { userId: 1 }); // lembrar que só o auth é suficiente
+}
+
+const api = { postLikeOrNot, getLikes };
+
+export default api;
