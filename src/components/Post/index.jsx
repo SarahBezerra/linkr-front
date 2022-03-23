5b077c235d446d27041049ca9dd9c2feea4681b8
Chaves.Rodrigo <rodrigoccavalcante@gmail.com>
2022-03-23 13:11:14 -0300

    fix conflicts

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,11 +1,24 @@
 import { Container } from "./style";
+import urlMetadata from "url-metadata";
 
 
 function Post(){
 
+    // const x = urlMetadata('https://www.npmjs.com/package/url-metadata');
+
+    // x.then(
+    //     function (metadata){
+    //         console.log(metadata);
+    //     },
+    //     function (error){
+    //        console.log(error);
+    //     },
+    // )
+
+
     return (
         <Container>
-                
+                teste de fonte
         </Container>
     )
 }
--- src/components/Post/style.jsx
+++ src/components/Post/style.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
+import urlMetadata from "url-metadata"
 
 const Container = styled.div`
     width: 600px;
     background-color: black;
     min-height: 300px;
     border-radius: 16px;
+    color: white;
 `;
 
 
