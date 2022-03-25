c2aa94bb83f1ca079da9102c962ee428f18a1e53
Chaves.Rodrigo <rodrigoccavalcante@gmail.com>
2022-03-25 10:27:21 -0300

    resolve conflict on App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from "react";
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Timeline from "./pages/Timeline";
+import React from 'react';
+import {useState} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Timeline from './pages/Timeline';
 import Header from "./components/Header/Header";
-import SignIn from "./pages/SignIn";
-import Hashtag from "./pages/Hashtag";
 import Content from "./components/PageLayout/Content";
+import SignIn from "./pages/SignIn";
+import Hashtag from './pages/Hashtag';
+//import { Container } from './components/Post/style';
+
 
 function App() {
+
   const [menuSelect, setMenuSelect] = useState(false);
+  
 
   return (
         <Content>
           <BrowserRouter>
             <Header menuSelect={menuSelect} setMenuSelect={setMenuSelect}/>
-            <Routes>
-              <Route path='/timeline' element={<Timeline />} />
-              <Route path='/login' element={<SignIn/>}></Route>
-              <Route path='/sign-up' element={<SignIn/>}></Route>
-              <Route path='/hashtag/:hashtag' element={<Hashtag/>}/>
-            </Routes>
+              <Routes>
+                {/* <Container> */}
+                  <Route path='/timeline' element={<Timeline />} />
+                  <Route path='/login' element={<SignIn/>}></Route>
+                {/* </Container> */}
+                <Route path='/sign-up' element={<SignIn/>}></Route>
+                <Route path='/hashtag/:hashtag' element={<Hashtag /> }/>
+              </Routes>
+            
           </BrowserRouter>
         </Content>
   )
