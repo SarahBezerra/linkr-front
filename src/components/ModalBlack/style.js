dc5a1266384e7437ba05c93cbeea44781870f985
Yara Tavares <yaracriistavares@gmail.com>
2022-03-27 17:27:09 -0300

    style: new modal in click trash

---
+++ src/components/ModalBlack/style.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+const stylizedModal = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+
+    width: "597px",
+    height: "262px",
+    backgroundColor: "#333333",
+    borderRadius: "50px",
+
+    textAlign: "center",
+    border: "none",
+  },
+
+  overlay: {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+  },
+};
+
+const InsideModal = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h2 {
+    width: 70%;
+    line-height: 41px;
+    font-weight: 700;
+    font-size: 34px;
+    color: white;
+  }
+`;
+
+const BoxButtons = styled.div`
+  margin-top: 39px;
+
+  button {
+    width: 134px;
+    height: 37px;
+
+    border: none;
+    border-radius: 5px;
+    font-size: 18px;
+
+    :first-child {
+      margin-right: 27px;
+      color: #1877f2;
+      background: white;
+    }
+
+    :last-child {
+      color: white;
+      background-color: #1877f2;
+    }
+  }
+`;
+
+export { stylizedModal, InsideModal, BoxButtons };
--- src/components/TrashAndEdit/index.js
+++ src/components/TrashAndEdit/index.js
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import { PencilSharp, Trash } from "react-ionicons";
+import ReactModal from "react-modal";
+
 import api from "../../services/api";
 import useAuth from "../../hooks/useAuth";
-import BoxIcons from "./style";
+import { BoxIcons } from "./style";
+import ModalBlack from "../ModalBlack";
+
+ReactModal.setAppElement("#root");
+// Modal.setAppElement("#root");
+
+const customStyles = {
+  content: {},
+};
 
 export default function TrashAndEdit({ infos }) {
   const { auth } = useAuth();
+  const [wait, setWait] = useState(false);
+  const [modal, setModal] = useState(false);
+
   async function handleClickDelete() {
-    const confirm = window.confirm("Deseja deletar?");
-
-    if (!confirm) return;
-
+    setWait(true);
     try {
       await api.deletePost(infos.id, auth.token);
-      alert("deletado com sucesso");
+      setModal(false);
+      setWait(false);
     } catch (err) {
       console.log("aconteceu um erro em delete");
     }
@@ -20,6 +32,13 @@ export default function TrashAndEdit({ infos }) {
 
   return (
     <BoxIcons>
+      <ModalBlack
+        loading={wait}
+        showModal={modal}
+        setShowModal={setModal}
+        confirmFunction={handleClickDelete}
+        questionTitle={"Are you sure you want to delete this post?"}
+      />
       <PencilSharp
         color={"#FFFFFF"}
         rotate={false}
@@ -33,7 +52,7 @@ export default function TrashAndEdit({ infos }) {
         shake={false}
         height="14px"
         width="14px"
-        onClick={() => handleClickDelete()}
+        onClick={() => setModal(true)}
       />
     </BoxIcons>
   );
